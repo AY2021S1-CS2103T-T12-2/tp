75f542707675f4f3194b982da940a33ee020a735
Qian <35624031+Persdre@users.noreply.github.com>
2020-11-08 00:36:28 +0800

    Update PhoneTest.java

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -37,6 +37,5 @@ public class PhoneTest {
         // valid phone numbers
         assertTrue(Phone.isValidPhone("91107866"));
         assertTrue(Phone.isValidPhone("93121534"));
-        assertTrue(Phone.isValidPhone("80967766")); 
     }
 }
